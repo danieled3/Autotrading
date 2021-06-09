5c85fe62f749903a2e66a6a94c1cac557b562db7
Daniele <daniele.dan@outlook.com>
2021-06-09 02:10:08 +0200

    added trading.py

--- training.py
+++ training.py
@@ -5,14 +5,17 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
+import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
 import pickle
 
 # Select symbol to predict
 symbol = 'TSLA_close'
-max_features_number = 60 #limit the max number of features to use becuase we will can download a limited amount of data because of API boundary
+max_features_number = 60  # limit the max number of features to use, we can download a limited amount of
+# data because of API boundary
 train_split = 0.8
 days_to_predict = [5,10,15,20]
-window_size = 40
+window_size = 3
 
 # Load best features and best offset list
 with open('features_selector_data/features_selector.p', 'rb') as fp:
@@ -35,55 +38,86 @@ else:
 # Create matrices for x and y where y[time] and x[time,feature]
 y = np.flip(np.array(full_data[symbol]))  # y[time]
 x = np.array(full_data[features])
-for i in range(x.shape[1]):   #  x[time,feature]
-    x[:,i] = np.flip(x[:,i])
+for i in range(x.shape[1]):  # x[time,feature]
+    x[:, i] = np.flip(x[:, i])
 
 # Apply offset to each features in x and reshape matrix
-series_length = [ (x.shape[0] - offsets[i] - sum(np.isnan(x[i])))
-                  for i in range(len(features))
-                  ]
+series_length = [(x.shape[0] - offsets[i] - sum(np.isnan(x[i])))
+                 for i in range(len(features))
+                 ]
 x_length = min(series_length)
-offset_y= y[-x_length :]
+offset_y = y[-x_length:]
 offset_x = np.empty((x_length, len(features)))
 
 for i in range(len(features)):
-    offset_x[:,i] = x[:,i][-offsets[i]-x_length : -offsets[i]]
+    offset_x[:, i] = x[:, i][-offsets[i] - x_length: -offsets[i]]
 
 max_predicted_period = max(days_to_predict)
 window_x = [None] * (x_length - window_size)
 window_y = [None] * (x_length - window_size)
 
 for i in range(x_length - window_size):
-    current_window_x = offset_x[i+1:i+window_size+1,:]
-    current_window_y = offset_y[i+window_size - max_predicted_period+np.array(days_to_predict)]
+    current_window_x = offset_x[i + 1:i + window_size + 1, :]
+    current_window_y = offset_y[i + window_size - max_predicted_period + np.array(days_to_predict)]
     window_x[i] = current_window_x
     window_y[i] = current_window_y
 
 window_x = np.array(window_x)
 window_y = np.array(window_y)
 
-# Use the oldes values ofr training and the newest values for validation
-index_split = int(train_split * windw_x.shape[0])
-x_train= window_x[:index_split,:,:]
-x_valid= window_x[index_split:,:,:]
-y_train= window_y[:index_split,:]
-y_valid= window_y[index_split:,:]
+# Use the oldest values ofr training and the newest values for validation
+index_split = int(train_split * window_x.shape[0])
+x_train = window_x[:index_split, :, :]
+x_valid = window_x[index_split:, :, :]
+y_train = window_y[:index_split, :]
+y_valid = window_y[index_split:, :]
 
-# Build model
-model = tf.keras.models.Sequential([
-  #tf.keras.layers.Conv1D(filters=32, kernel_size=5, strides=1, padding="causal", activation="relu",input_shape=[None, 1]),
-  tf.keras.layers.LSTM(64, input_shape = (30, window_size, len(features))),
-  tf.keras.layers.LSTM(64, return_sequences=True),
-  tf.keras.layers.Dense(30, activation="relu"),
-  tf.keras.layers.Dense(10, activation="relu"),
-  tf.keras.layers.Dense(1),
-  tf.keras.layers.Lambda(lambda x: x * 400) # depends on data
-])
+go_on = True
+while (go_on):
+    model = tf.keras.models.Sequential([
+        tf.keras.layers.Conv1D(filters=32, kernel_size=5,
+                               strides=1, padding="causal",
+                               activation="relu",
+                               input_shape=[window_size, len(features)]),
+        tf.keras.layers.LSTM(64, return_sequences=True),
+        tf.keras.layers.LSTM(64),
+        tf.keras.layers.Dense(64, activation="relu"),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Dense(10, activation="relu"),
+        tf.keras.layers.Dense(len(days_to_predict)),
+        tf.keras.layers.Lambda(lambda x: x * max(y))  # depends on data
+    ])
 
-optimizer = tf.keras.optimizers.SGD(lr=1e-5, momentum=0.9)
-model.compile(loss=tf.keras.losses.Huber(),
-              optimizer=optimizer,
-              metrics=["mae"])
-history = model.fit(x_train, y_train, validation_data = (x_valid, y_valid),epochs=10)
+    optimizer = tf.keras.optimizers.Adam(lr=1e-5)
+    model.compile(loss=tf.keras.losses.Huber(),
+                  optimizer=optimizer,
+                  metrics=["mae"])
+    checkpoint_cb = tf.keras.callbacks.ModelCheckpoint('models/' + symbol + '_4pred.h5', save_best_only=True)
+    early_stopping_cb = tf.keras.callbacks.EarlyStopping(patience=300, restore_best_weights=True)
 
+    history = model.fit(
+        x_train, y_train,
+        validation_data=(x_valid, y_valid),
+        epochs=5000,
+       callbacks=[checkpoint_cb, early_stopping_cb]
+    )
 
+    if  (history.history['val_mae'][-300] < 80):
+        go_on = False
+
+# Plot training and validation loss
+loss = history.history['loss']
+val_loss = history.history['val_loss']
+epochs=range(len(loss)) # get number of epochs
+
+plt.plot(epochs, loss, 'r', label = 'Training Loss')
+plt.plot(epochs, val_loss, 'b', label = 'Validation Loss')
+plt.vlines(x=len(loss) - 300, ymin=0, ymax=600,  # add early stopping vertical line
+           colors='green', ls=':', lw=2,
+           label='Early stopping')
+plt.title('Training and Validation Loss')
+plt.xlabel("Epochs")
+plt.ylabel("Loss")
+
+plt.legend()
+plt.figure()
